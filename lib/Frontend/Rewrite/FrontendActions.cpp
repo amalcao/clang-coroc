cc348e565922fd7452a5513844367d549fc29913
Amal Cao <amalcaowei@gmail.com>
2014-07-28 15:26:56 +0800

    Fix compiling errors.

--- lib/Frontend/Rewrite/FrontendActions.cpp
+++ lib/Frontend/Rewrite/FrontendActions.cpp
@@ -166,6 +166,18 @@ ASTConsumer *RewriteObjCAction::CreateASTConsumer(CompilerInstance &CI,
 
 #endif
 
+
+//===----------------------------------------------------------------------===//
+// Rewrite-CoroC Actions
+//===----------------------------------------------------------------------===//
+ASTConsumer *RewriteCoroCAction::CreateASTConsumer(CompilerInstance &CI,
+												   StringRef InFile) {
+  if (raw_ostream *OS = CI.createDefaultOutputFile(false, InFile, "cc"))
+    return CreateCoroCRewriter(InFile, OS,
+							   CI.getDiagnostics(), CI.getLangOpts());
+  return nullptr;
+}
+
 //===----------------------------------------------------------------------===//
 // Preprocessor Actions
 //===----------------------------------------------------------------------===//
